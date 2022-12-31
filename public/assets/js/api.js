5fe4711fe219905722197ce9cfb4f33e0bf0906e
Artur Bomtempo Colen <arturbcolen@gmail.com>
2022-12-31 16:36:48 -0300

    Update api.js

--- public/assets/js/api.js
+++ public/assets/js/api.js
@@ -1,8 +1,5 @@
-// Variable containing the key of the API used
+// Import information from the API
 let api_key = '6dca09b18a823198985ff989b8ad1431';
-// Variable to import the images 
 let image_url = 'https://image.tmdb.org/t/p/w500';
-// Variable to import movie genres
 let genres_list_http = 'https://api.themoviedb.org/3/genre/movie/list?';
-// Variable to import the movies 
 let movie_genres_http = 'https://api.themoviedb.org/3/discover/movie?';
