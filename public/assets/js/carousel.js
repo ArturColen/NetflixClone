721144399c13b726ccca8d518dde546747e32597
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-06-19 09:35:18 -0300

    Update carousel.js

--- public/assets/js/carousel.js
+++ public/assets/js/carousel.js
@@ -1,5 +1,4 @@
 const setupScrolling = () => {
-    // Select the elements to be manipulated
     const container = [...document.querySelectorAll('.movie-container')];
     const nextBtn = [...document.querySelectorAll('.next-btn')];
     const prevBtn = [...document.querySelectorAll('.pre-btn')];
@@ -8,7 +7,6 @@ const setupScrolling = () => {
         let containerDimensions = item.getBoundingClientRect();
         let containerWidth = containerDimensions.width;
 
-        // Create the carousel scrolling event when clicking the buttons
         nextBtn[i].addEventListener('click', () => {
             item.scrollLeft += containerWidth;
         });
