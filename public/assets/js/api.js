d97aefc06c16449f47bc2d83a7bce33b805de084
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-06-19 09:35:01 -0300

    Update api.js

--- public/assets/js/api.js
+++ public/assets/js/api.js
@@ -1,4 +1,3 @@
-// Import information from the API
 let api_key = '6dca09b18a823198985ff989b8ad1431';
 let image_url = 'https://image.tmdb.org/t/p/w500';
 let genres_list_http = 'https://api.themoviedb.org/3/genre/movie/list?';
