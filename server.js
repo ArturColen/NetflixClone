c37b58e2086774a47d66947110349f4ebc2110bb
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-12-07 15:21:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
-// Import express and path
 const express = require('express')
 const path = require('path')
 
-// Storing the application port
 const port = 3333
 
-// Declare an initial path
 let initialPath = path.join(__dirname, 'public')
-// Declare a server application instance
 let app = express()
 app.use(express.static(initialPath))
 
-// Choose application input file
 app.get('/', (req, res) => {
     res.sendFile(path.join(initialPath, 'index.html'))
 })
 
-// Start server
 app.listen(port, () => {
     console.log(`Server start up on port ${port}!`)
 })
